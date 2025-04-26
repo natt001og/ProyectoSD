c9d22868f3016067cb2cdea4ad4f4cfa4b7220dc
isidxraaaa <isidoragonzalezm@hotmail.com>
2025-04-26 10:29:33 -0400

    Update scraper.py

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -4,19 +4,19 @@ from pymongo import MongoClient
 
 client = MongoClient("mongodb://mongo:27017/")
 db = client["trafico"]
-collection = db["eventos"]
+collection = db["eventos3"]
 
 
 def obtener_eventos_waze():
     url = "https://www.waze.com/live-map/api/georss"
     # Configuración para scrapear el centro
     params = {
-        "top": -33.4250,
-        "bottom": -33.4650,
-        "left": -70.6950,
-        "right": -70.6150,
+        "top": -34.983312784762894,
+        "bottom": -34.98977870345623,
+        "left": -71.2445515394211,
+        "right": -71.22054040431978,
         "env": "row",
-        "types": "alerts,traffic,users"
+        "types": "alerts"
     }
     headers = {
         "User-Agent": "Mozilla/5.0",
@@ -28,7 +28,7 @@ def obtener_eventos_waze():
         response = requests.get(url, params=params, headers=headers)
         if response.status_code == 200:
             data = response.json()
-            eventos = data.get("alerts", []) + data.get("irregularities", [])
+            eventos = data.get("alerts", [])
             if eventos:
                 collection.insert_many(eventos)
                 eventos_totales += len(eventos)
